21781e3a4a9fa64feea69e38fc09b79eee650c54
Rahul-Bhatt03 <rahulbhatt.fs@gmail.com>
2025-12-19 13:18:01 +0545

    working on home page layout

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,36 @@
+import { lazy, Suspense } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
+// const Home = lazy(() => import('../pages/Products').then(module => ({ default: module.Products })))
+// const Users = lazy(() => import('../pages/User').then(module => ({ default: module.Users })));
+// const Medicines = lazy(() => import('../pages/Medicines').then(module => ({ default: module.Medicines })));
+
+const LoadingSpinner = () => (
+    <div className="flex items-center justify-center h-64">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
+    </div>
+);
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        // element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: (
+                    <Suspense fallback={<LoadingSpinner />}>
+                        {/* <Home /> */}
+                    </Suspense>
+                )
+            },
+          
+           
+        ]
+    }
+])
+
+export const AppRouter = () => {
+    return <RouterProvider router={router} />
+}
