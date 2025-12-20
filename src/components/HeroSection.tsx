a24db1b71bf19aaeb33ebeb11865d6b4eeb2034e
Rahul-Bhatt03 <rahulbhatt.fs@gmail.com>
2025-12-20 22:25:36 +0545

    fixed the navbar position

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ export const HeroSection = () => {
   return (
     <section className="relative w-full h-screen overflow-hidden">
       
-      <div className="absolute top-0 left-0 right-0 z-50">
+      <div className="absolute top-10 left-0 right-0 z-50">
         <Navbar />
       </div>
 
